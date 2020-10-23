23e6354ec26c0354aee14abf7ddbe0ff161df560
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-10-23 15:50:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,11 +29,6 @@ class Bird {
   }
 
   speak() {
-    // if (this.sex === "male") {
-    //   return `It's me! ${this.name}, the parrot!`;
-    // } else {
-    //   return `${this.name} says squawk!`;
-    // }
     switch(this.sex) {
       case "male":
         return `It's me! ${this.name}, the parrot!`;
